8735a268230f7e490713851e99b442a406eaad77
Aasim Ahmed <42317288+aasimahmed@users.noreply.github.com>
2019-05-06 21:47:56 +0100

    Add files via upload

---
+++ StudentTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StudentTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StudentTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("612c49cc-0b81-4da2-a055-6c6ff2d08daa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StudentTest/UnitTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Q1_Exercise14;
+
+namespace StudentTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestConstructorStudentNameOnly()
+        {
+
+            //Test access to properties
+            //AAA, arrange acy assery
+
+            Student student = new Student("Aasim", "Ahmed");
+
+
+            Assert.AreEqual(student.Firstname, "Aasim");
+            Assert.AreEqual(student.Lastname, "Ahmed");
+            Assert.IsNull(student.Email);
+            Assert.IsNull(student.Course);
+        }
+
+        [TestMethod]
+
+        public void TestConstructorFullStudent()
+        {
+            Student student = new Student("Aasim", "Middle", "Ahmed" , "Computer Science", "Algorithms", "Harvard", "na@na.com", 123456789);
+
+            Assert.AreEqual(student.TelephoneNumber, 123456789); // trailing 0 causes a problem here
+            Assert.AreEqual(student.Email, "na@na.com");
+            
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestConstructorFailure()
+        {
+            Student student = new Student();
+            
+
+            
+            
+        }
+
+        [TestMethod]
+        public void CreateMockStudents()
+        {
+            Student[] students = Student.CreateMockStudents();
+
+            Assert.AreEqual(students[4].Firstname, "Jay");
+            
+            
+
+            
+        }
+        [TestMethod]
+        public void CheckSingletonMockStudents()
+        {
+            Student[] students = Student.CreateMockStudents();
+            Student[] studentChecker = Student.CreateMockStudents();
+
+            Assert.IsTrue(Object.ReferenceEquals(students, studentChecker));
+            
+
+        }
+
+    }
+}
